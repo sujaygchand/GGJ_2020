94729b791b060639f44e97d57e33a46764595bd6
Sujay Chand <sujaygchand@gmail.com>
2020-02-01 13:04:51 +1300

    Update Spawner

---
+++ Assets/Scripts/Game Managers/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public List<RuinScript> ruins;
+    public List<RuinSpawnPoint> ruinSpawnPoints;
+    public int ruinsToSpawn;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        SpawnRuins();
+
+        if (ruins.Count < 1)
+        {
+            Debug.LogError("ruins array is empty");
+            return;
+        }
+
+    }
+
+    // Update is called once per frame
+    private void SpawnRuins()
+    {
+        ruinSpawnPoints = ruinSpawnPoints.OrderBy(spawnPoint => Random.value).ToList();
+    }
+}

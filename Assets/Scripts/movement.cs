71ee497d78673061bd9dc7b05d98f2f4176e9112
Sujay Chand <sujaygchand@gmail.com>
2020-02-01 16:08:38 +1300

    Started on input

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
+
+    public static string moveForward = "MoveForward_P";
+    public static string moveRight = "MoveRight_P";
+
     // Start is called before the first frame update
     CharacterController characterController;
     public float speed = 6.0f;
@@ -11,16 +15,24 @@ public class movement : MonoBehaviour
     public float gravity = 20.0f;
     //public Rigidbody moverthing;
     private Vector3 moveDirection = Vector3.zero;
+
+    private string moveForwardAxis;
+    private string moveRightAxis;
+
+    [Range(1,4)]
+    public int playerNum = 1;
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        moveForwardAxis = moveForward + playerNum;
+        moveRightAxis = moveRight + playerNum;
     }
 
     
     // Update is called once per frame
     void Update()
     {
-        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        moveDirection = new Vector3(Input.GetAxis(moveForwardAxis), 0.0f, Input.GetAxis(moveRightAxis));
         moveDirection *= speed;
         characterController.Move(moveDirection * Time.deltaTime);
     }

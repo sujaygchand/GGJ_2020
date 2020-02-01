4800fa50a86cebafddc54228d1167509a31464e4
Levi <1>
2020-02-01 14:36:38 +1300

    added demolition and rewind (enter)

---
+++ Assets/Scripts/TimeBody.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeBody : MonoBehaviour
+{
+    public bool isRewinding = false;
+    public MeshCollider mesh;
+
+    List<PointInTime> pointsintime;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = GetComponent<MeshCollider>();
+        pointsintime = new List<PointInTime>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            StartRewind();
+
+        }
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            StopRewind();
+        }
+    }
+    void FixedUpdate()
+    {
+        if (isRewinding)
+        {
+            Rewind();
+        }
+        else
+            Record();
+    }
+    void Rewind()
+    {
+        if (pointsintime.Count > 0)
+        {
+            PointInTime pointInTime = pointsintime[0];
+            transform.position = pointInTime.position;
+            transform.rotation = pointInTime.rotation;
+            pointsintime.RemoveAt(0);
+        }
+        else
+        {
+            StopRewind();
+        }
+            
+        
+    }
+    void Record()
+    {
+
+        pointsintime.Insert(0, new PointInTime(transform.position,transform.rotation));
+    }
+    public void StartRewind()
+    {
+        isRewinding = true;
+        rb.isKinematic = true;
+        mesh.enabled = true;
+        rb.constraints = RigidbodyConstraints.None;
+    }
+    public void StopRewind()
+    {
+        isRewinding = false;
+        rb.isKinematic = false;
+        rb.constraints = RigidbodyConstraints.FreezePosition;
+        mesh.enabled = false;
+    }
+}

d4dd25a6ff2a06f4af5b14cceb9a618c3bedc1f2
ari <annabelleritchie42@gmail.com>
2020-02-01 15:02:27 +1300

    Start menu initial changes

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitGame : MonoBehaviour
+{
+    [SerializeField]
+    private Button quitButton;
+
+    private void Awake()
+    {
+        quitButton = quitButton ?? GetComponent<Button>();
+
+        if (quitButton == null) {
+            return;
+        }
+
+        quitButton.onClick.AddListener(() => QuitGame());
+
+    }
+    public void QuitGame()
+    {
+        Application.Quit(); 
+    }
+}
